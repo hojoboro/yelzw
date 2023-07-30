3ba09af7006a093fa15c62b6cd6b5337e29bc86e
CrazyboyOfficial <139047964+CrazyboyOfficial@users.noreply.github.com>
2023-07-30 16:44:00 -0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={1808873617}'>CrazyboyOfficial</a>\n○ Made By : <code>CrazyboyOfficial</code>\n○ Copyright : <a href='https://telegram.me/RBMPrime_bot'>Click Here</a>\n○ Source Code : <a href='https://telegram.me/RBMPrime_Bot'>Click here</a>\n○ Channel : @CrazyboyOfficial\n○ To Create Your Own Bot Like This Text : @RBMPrime_Bot</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={1808873617}'>CrazyboyOfficial</a>\n○ Made By : <code>CrazyboyOfficial</code>\n○ Copyright : <a href='https://telegram.me/RBMPrime_bot'>Click Here</a>\n○ Any Issue : <a href='https://telegram.me/RBMPrime_Bot'>Click Here</a>\n○ Channel : @CrazyboyOfficial\n○ To Create Your Own Bot Like This Text : @RBMPrime_Bot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
